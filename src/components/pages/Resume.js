f796c0cf55f8fdb895dddd558d6fbc97f38545f8
hopemansfield <hopemansfield@gmail.com>
2023-06-08 12:22:38 -0400

    updated resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,10 +1,24 @@
 import React from "react";
 
+const styles = {
+  p: {
+    textAlign: "center",
+  },
+};
+
 export default function Resume() {
   return (
     <div>
       <h2>Resume</h2>
-      <h2>COMING SOON!</h2>
+      <p style={styles.p}>
+        <a
+          href="https://docs.google.com/document/d/1wyYNpiCdYzQSeXANp-hqYGYV0h5GeGNd/edit?usp=sharing&ouid=106744623781344894069&rtpof=true&sd=true"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Click here to view my resume!
+        </a>
+      </p>
     </div>
   );
 }
